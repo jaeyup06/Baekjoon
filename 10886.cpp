78247d9f729213caa8a24c67bd5b295e4415af0e
jaeyup06 <jaeyup06@gmail.com>
2025-04-06 12:39:41 +0900

    10886 clear

---
+++ 10886.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int people, yes = 0, no = 0;
+
+    cin >> people;
+
+    for (int comments; 0 < people; people--)
+	{
+        cin >> comments;
+
+        if (comments == 1)
+            yes++;
+        else
+            no++;
+    }
+
+    if (yes > no)
+        cout << "Junhee is cute!" << endl;
+    else
+        cout << "Junhee is not cute!" << endl;
+
+    return 0;
+}
