a2ef55dba141e82fd62f085a3b7eb60c67c1d1b9
jaeyup06 <jaeyup06@gmail.com>
2025-04-06 12:41:10 +0900

    10250 clear

---
+++ 10250.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+
+    cin >> T;
+
+    while (T > 0)
+	{
+        int H, W, N;
+
+        cin >> H >> W >> N;
+
+        int X = (N - 1) % H + 1;
+        int Y = (N - 1) / H + 1;
+
+        cout << X * 100 + Y << endl;
+        T--;
+    }
+
+    return 0;
+}
