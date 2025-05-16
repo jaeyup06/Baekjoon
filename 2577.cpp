1ab728d47dd983c37a3af1926b0e21f66ce71534
재엽 <jaeyup06@gmail.com>
2025-05-16 10:04:53 +0900

    2577 clear

---
+++ 2577.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, a, b, c;
+    int count[10] = {0};
+
+    cin >> a >> b >> c;
+
+    num = a * b * c;
+
+    while (num > 0) {
+        int i = num % 10;
+        count[i]++;
+        num /= 10;
+    }
+
+    for(int i = 0; i < 10; i++) {
+        cout << count[i] << endl;
+    }
+
+    return 0;
+}
