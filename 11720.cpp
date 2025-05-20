f5e179a6d6151e3d8a4d85c0ed3fea78f27c905c
재엽 <jaeyup06@gmail.com>
2025-05-20 20:50:59 +0900

    11720 clear

---
+++ 11720.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, sum = 0;
+    string numbers;
+    cin >> N >> numbers;
+
+    for(int i = 0; i < N; i++) {
+        sum += numbers[i] - '0';
+    }
+
+    cout << sum;
+
+    return 0;
+}
