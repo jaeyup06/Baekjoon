8f7ff98c7030d50062d271aa279c4749ae2c9e6d
재엽 <jaeyup06@gmail.com>
2025-05-26 14:40:17 +0900

    18238 clear

---
+++ 18238.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+
+    char last_char = 'A';
+    int total_time = 0;
+
+    for(int i = 0; i < S.length(); i++) {
+        if(abs(last_char - S[i]) > 26 - abs(last_char - S[i])) {
+            total_time += 26 - abs(last_char - S[i]);
+        }
+        else {
+            total_time += abs(last_char - S[i]);
+        }
+        last_char = S[i];
+    }
+
+    cout << total_time;
+
+    return 0;
+}
